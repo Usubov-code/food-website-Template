030f4d1b48bff344f4721f737a7cb8ead1a98545
Elshad Usubov <92145698+Usubov-code@users.noreply.github.com>
2021-12-15 17:22:57 +0400

    Add project files.

---
+++ food-website-Template/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using food_website_Template.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace food_website_Template.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+     
+
+    }
+}
